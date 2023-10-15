e5487ce72f805e1e845e55a5c7d0cbb39c6a1399
willowitzy <willowitzy@gmail.com>
2023-10-14 22:38:27 -0300

    aprimorando o programa leitura

--- leitura.c
+++ leitura.c
@@ -1,37 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <locale.h>
 
+/* Argumentos de linha de comando em C
+    argc: o número de argumentos passados para o programa na linha de comando.
+    O primeiro argumento é o nome do programa em si, e o valor mínimo de argc é 1 (indicando apenas o nome do programa).
+    argv[]: : Um array de strings (ou seja, um array de ponteiros para caracteres) que contém os argumentos passados para o programa. 
+    argv[0] normalmente contém o nome do programa e argv[1] em diante contém os argumentos adicionais.
+*/
 int main(int argc, char* argv[]) {
-    int indiceLeitura;
+    //setlocale(LC_ALL, "Portuguese");
+    // No PowerShell, usa esse comando p/ poder exibir caracteres latinos: 65001
+
+    // Debug - pode comentar / apagar
+    printf("Número de argumentos: %d\n", argc);
+
+    for (int i = 0; i < argc; i++) {
+        printf("Argumento %d: %s\n", i, argv[i]);
+    }
+
+    int indicedoComandoLeitura;
     int existeIndiceL = 0;
     char ch;
 
-    // loop para identiicar quantidade de argumentos.
-    for(int i = 0; i < argc; i++){
-        if(strcmp(argv[i], "./leitura") == 0){
-            indiceLeitura = i;
-            printf("%i\n", indiceLeitura);
-            existeIndiceL++;
+    // Verificando se existe um segundo argumento
+    for(int i = 0; i < argc; i++) {
+        if (i >= 1) {
+            existeIndiceL = 1;
         }
     }
 
     if(existeIndiceL){
-        for(int i = indiceLeitura + 1; i < (indiceLeitura + 2); i++){
-            printf("%s", argv[i]);       
-            FILE *dados_leitura = fopen(argv[i], "r");
-            FILE* dados_escrita = fopen("relatorio.txt", "w");
+        indicedoComandoLeitura = 1; 
+            FILE *dados_leitura = fopen(argv[indicedoComandoLeitura], "r");
+
+            // Verificando se o arquivo foi aberto com sucesso
+            if (dados_leitura == NULL) {
+                printf("Erro ao abrir o arquivo!\n");
+                return 1;
+            }
+
+            FILE *dados_escrita = fopen("relatorio.txt", "w");
+            
+            // Verificando se o arquivo relatorio.txt foi gerado com sucesso
+            if (dados_escrita == NULL) {
+                printf("Erro ao criar o arquivo de saída!\n");
+                return 1;
+            }
+
+            /*
             while (!feof(dados_leitura)) {
                 ch = fgetc(dados_leitura);
                 printf("%c", ch);
 
                 fputc(ch, dados_escrita);
             }
-        }
+            */
+
+            while ((ch = fgetc(dados_leitura)) != EOF) {
+                fputc(ch, dados_escrita);
+            }
+
+            fclose(dados_leitura);
+            fclose(dados_escrita);
+
         return 0;
     }
     else {
-        printf("Adicione o comando leitura!");
+        printf("Adicione o arquivo CSV para ser lido pelo programa!\n");
 
         return 1;
     }
