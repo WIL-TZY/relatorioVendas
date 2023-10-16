6475278fcc3512a49b6df92b2641826f0039c423
willowitzy <willowitzy@gmail.com>
2023-10-16 20:54:27 -0300

    renomeando um bocado

--- analise.c
+++ analise.c
@@ -5,24 +5,31 @@
 // Esse programa vai montar o relatório de vendas e disponibilizar os dados para um terceiro programa que irá criar o relatório final.
 // Não recebe parâmetros.
 
+// Valores --- Comissão
+const double COMISSAO_JUNIOR = 0.01;
+const double COMISSAO_PLENO = 0.02;
+const double COMISSAO_SENIOR = 0.03;
+const double COMISSAO_GERENTE = 0.05;
+
 typedef struct {
     int id_vendedor;
-    char nome[100];
+    char nomeVendedor[100];
     char cargo[20];
     int equipe;
     int id_venda;
     double valor_venda;
-} Vendedor;
+    double comissao;
+} DefinaVenda;
 
 // ------------------------------------ Protótipos das funções aqui ------------------------------------ //
-double calcularTotalVendas(Vendedor vendedores[], int quantidadeVendedores);
-void ordenarVendedoresPorVendas(Vendedor vendedores[], int quantidadeVendedores);
-void gerarRelatorio(Vendedor vendedores[], int quantidadeVendedores);
+double calcularTotalVendas(DefinaVenda venda[], int qtdVendas);
+void ordenarVendedoresPorVendas(DefinaVenda venda[], int qtdVendas);
+void gerarRelatorio(DefinaVenda venda[], int qtdVendas);
 // ----------------------------------------------------------------------------------------------------- //
 
 int main() {
-    Vendedor vendedores[100];
-    int quantidadeVendedores = 0;
+    DefinaVenda venda[100];
+    int linha = 0;
     int colunasCSV = 6;
     char bufferLinhas[100]; // Esse buffer vai ser usado temporariamente para armazenar os dados pegos do STDIN
     int contadorDeChar = 0;
@@ -42,32 +49,32 @@ int main() {
             // Armazenar cada informação separada por vírgulas dentro de token
             char *token = strtok(bufferLinhas, ",");
             int col = 0;
-            // Preencher o array de vendedores com os dados do arquivo CSV
+            // Preencher o array de venda com os dados do arquivo CSV
             while (token != NULL && col < colunasCSV) {
                 if (col == 0) {
-                    vendedores[quantidadeVendedores].id_vendedor = atoi(token);
+                    venda[linha].id_vendedor = atoi(token);
                 } else if (col == 1) {
-                    strncpy(vendedores[quantidadeVendedores].nome, token, sizeof(vendedores[quantidadeVendedores].nome) - 1);
-                    vendedores[quantidadeVendedores].nome[sizeof(vendedores[quantidadeVendedores].nome) - 1] = '\0';
+                    strncpy(venda[linha].nomeVendedor, token, sizeof(venda[linha].nomeVendedor) - 1);
+                    venda[linha].nomeVendedor[sizeof(venda[linha].nomeVendedor) - 1] = '\0';
                 } else if (col == 2) {
-                    strncpy(vendedores[quantidadeVendedores].cargo, token, sizeof(vendedores[quantidadeVendedores].cargo) - 1);
-                    vendedores[quantidadeVendedores].cargo[sizeof(vendedores[quantidadeVendedores].cargo) - 1] = '\0';
+                    strncpy(venda[linha].cargo, token, sizeof(venda[linha].cargo) - 1);
+                    venda[linha].cargo[sizeof(venda[linha].cargo) - 1] = '\0';
                 } else if (col == 3) {
-                    vendedores[quantidadeVendedores].equipe = atoi(token);
+                    venda[linha].equipe = atoi(token);
                 } else if (col == 4) {
-                    vendedores[quantidadeVendedores].id_venda = atoi(token);
+                    venda[linha].id_venda = atoi(token);
                 } else if (col == 5) {
-                    vendedores[quantidadeVendedores].valor_venda = atof(token);
+                    venda[linha].valor_venda = atof(token);
                 }
                 token = strtok(NULL, ",");
                 col++;
             }
                 // Próxima linha (incrementa o vendedor)
-                quantidadeVendedores++;
+                linha++;
 
                 // Checagem de segurança
-                if (quantidadeVendedores >= 100) {
-                    printf("Quantidade de vendedores excedida.\n");
+                if (linha >= 100) {
+                    printf("Quantidade de venda excedida.\n");
                     return 1;
                 }
         }
@@ -75,19 +82,19 @@ int main() {
 
     // ------------------------------------ USAR FUNÇÕES AQUI ------------------------------------ //
     
-    // (DEBUG) Imprimindo os dados de cada vendedor, agora armazenados na struct
-    for (int i = 0; i < quantidadeVendedores; i++) {
-        printf("Vendedor %d:\n", i + 1);
-        printf("ID: %d\n", vendedores[i].id_vendedor);
-        printf("Nome: %s\n", vendedores[i].nome);
-        printf("Cargo: %s\n", vendedores[i].cargo);
-        printf("Equipe: %d\n", vendedores[i].equipe);
-        printf("ID da venda: %d\n", vendedores[i].id_venda);
-        printf("Valor da venda: %.2f\n", vendedores[i].valor_venda);
+    // (DEBUG) Imprimindo os dados de cada struct
+    for (int i = 0; i < linha; i++) {
+        printf("Venda %d:\n", i + 1);
+        printf("ID: %d\n", venda[i].id_vendedor);
+        printf("Nome: %s\n", venda[i].nomeVendedor);
+        printf("Cargo: %s\n", venda[i].cargo);
+        printf("Equipe: %d\n", venda[i].equipe);
+        printf("ID da venda: %d\n", venda[i].id_venda);
+        printf("Valor da venda: %.2f\n", venda[i].valor_venda);
         printf("\n");
     }
 
-        gerarRelatorio(vendedores, quantidadeVendedores);
+        gerarRelatorio(venda, linha);
 
 
     return 0;
@@ -96,27 +103,27 @@ int main() {
 // ------------------------------------ CRIAR FUNÇÕES AQUI ------------------------------------ //
 
 
-double calcularTotalVendas(Vendedor vendedores[], int quantidadeVendedores) {
+double calcularTotalVendas(DefinaVenda venda[], int qtdVendas) {
     double totalVendas = 0.0;
-    for (int i = 0; i < quantidadeVendedores; i++) {
-        totalVendas += vendedores[i].valor_venda;
+    for (int i = 0; i < qtdVendas; i++) {
+        totalVendas += venda[i].valor_venda;
     }
     return totalVendas;
 }
 
 // Outra função parecida com a de cima só que por equipe (?)
 
-void ordenarVendedoresPorVendas(Vendedor vendedores[], int quantidadeVendedores) {
+void ordenarVendedoresPorVendas(DefinaVenda venda[], int qtdVendas) {
     // Dar uma olhada em algoritmos de ordenação??
     // Ou só comparar valor_venda de um com outro já dá?
 }
 
-void gerarRelatorio(Vendedor vendedores[], int quantidadeVendedores) {
+void gerarRelatorio(DefinaVenda venda[], int qtdVendas) {
     // Calcular total de vendas
-    double totalVendas = calcularTotalVendas(vendedores, quantidadeVendedores);
+    double totalVendas = calcularTotalVendas(venda, qtdVendas);
 
     // Ordenar vendedores por vendas
-    ordenarVendedoresPorVendas(vendedores, quantidadeVendedores);
+    ordenarVendedoresPorVendas(venda, qtdVendas);
 
     // Títulos
     printf("------ Relatório de Vendas ------\n");
@@ -134,17 +141,17 @@ void gerarRelatorio(Vendedor vendedores[], int quantidadeVendedores) {
 
     // Lista de vendedores
     printf("Lista de Vendedores (por vendas decrescentes):\n");
-    printf("Nome\t\t\t\tCargo\t\tEquipe\t\tTotal de Vendas\t\tMaior Venda\t\tComissão\n");
+    printf("Nome\t\t\t\tCargo\t\tEquipe\t\tTotal de Vendas\t\tMaior DefinaVenda\t\tComissão\n");
 
-    for (int i = 0; i < quantidadeVendedores; i++) {
+    for (int i = 0; i < qtdVendas; i++) {
         // Formatar e escrever os detalhes para cada vendedor (usando tabs tava dando inconsistência)
         printf("%-30s %-20s %02d \t\tR$ %12.2f \tR$ %12.2f \tR$ %10.2f\n", 
-            vendedores[i].nome, 
-            vendedores[i].cargo, 
-            vendedores[i].equipe, 
-            vendedores[i].valor_venda, 
-            vendedores[i].valor_venda, 
-            (vendedores[i].valor_venda * 0.03)
+            venda[i].nomeVendedor, 
+            venda[i].cargo, 
+            venda[i].equipe, 
+            venda[i].valor_venda, 
+            venda[i].valor_venda, 
+            (venda[i].valor_venda * 0.03)
         );
     }
 }
